4c13fdd3827420e46570434aa8cb03f2c5874156
Gopal Amlekar <gopal.amlekar@gmail.com>
2017-09-12 11:36:52 +0530

    Use spaces for indent instead of tabs

--- section-3-2-blink-LED.py
+++ section-3-2-blink-LED.py
@@ -10,16 +10,16 @@ print("Setting Broadcom Mode")
 # Pin Setup:
 GPIO.setmode(GPIO.BCM) # Broadcom pin-numbering scheme
 
-GPIO.setup(ledPin, GPIO.OUT) 
+GPIO.setup(ledPin, GPIO.OUT)
 
 print("Here we go! Press CTRL+C to exit")
 try:
     while 1:
-            print ("OFF");
-            GPIO.output(ledPin, GPIO.LOW)
-            time.sleep(1.00)
-            print ("ON");            
-            GPIO.output(ledPin, GPIO.HIGH)
-            time.sleep(1) 
+        print ("OFF");
+        GPIO.output(ledPin, GPIO.LOW)
+        time.sleep(1.00)
+        print ("ON");
+        GPIO.output(ledPin, GPIO.HIGH)
+        time.sleep(1)
 except KeyboardInterrupt: # If CTRL+C is pressed, exit cleanly:
     GPIO.cleanup() # cleanup all GPIO
